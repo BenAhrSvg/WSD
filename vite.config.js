e73543cb0d4c7a30b59ed21c2c5a3767019d1251
BenAhrSvg <benahrw@gmail.com>
2026-05-10 18:43:56 +0200

    fix: add base path and github action for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-﻿import { defineConfig } from "vite"
+import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
 
@@ -7,4 +7,5 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  base: "/WSD/",
 })
